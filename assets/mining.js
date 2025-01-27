fd63925da31d07ba3cc9459c4a98248a60185684
aatomu <aatomu21263+git@gmail.com>
2025-01-28 01:14:56 +0900

    🚧 local miningできるように

--- assets/mining.js
+++ assets/mining.js
@@ -9,9 +9,9 @@
  */
 async function generateHash(nonce, prevHash, log) {
 	const obj = new TextEncoder().encode(JSON.stringify({
-			nonce: nonce,
-			prevHash: prevHash,
-			log: log
+		nonce: nonce,
+		prevHash: prevHash,
+		log: log
 	}))
 	let buf = await crypto.subtle.digest("SHA-512", obj)
 	return Array.from(new Uint8Array(buf)).map((v) => v.toString(16).padStart(2, "0")).join("")
@@ -27,36 +27,50 @@ function isMatchChainPrefix(hash) {
 
 async function main() {
 	const log = [{
-			text: "aaa"
+		text: "aaa"
 	}, {
-			text: "bbb"
+		text: "bbb"
 	}, {
-			text: "ccc"
+		text: "ccc"
 	},
 	]
 
 	let hashList = []
 	let prevHash = ""
-	console.time("ticker")
+
 	for (let i = 0; i < 10; i++) {
-			let nonce = 0
-			while (true) {
-					const hash = await generateHash(nonce,prevHash,log)
-					if (isMatchChainPrefix(hash)) {
-							console.log(`no.${i} nonce:${nonce}, hash:${hash}`)
-							console.timeLog("ticker")
-							prevHash=hash
-							hashList.push(hash)
-							break
-					}
-					if (nonce%10000==0) {
-					console.log(`no.${i} nonce:${nonce} calc...`)
-					console.timeLog("ticker")
+		let nonce = 0
+		while (true) {
+			const hash = await generateHash(nonce, prevHash, log)
+			if (isMatchChainPrefix(hash)) {
+				console.log(`no.${i} nonce:${nonce}, hash:${hash}`)
+				appendLog(`no.${i} nonce:${nonce}, hash:${hash}`)
+				prevHash = hash
+				hashList.push(hash)
+				break
 			}
-					nonce++
+			if (nonce % 10000 == 0) {
+				console.log(`no.${i} nonce:${nonce} calc...`)
+				appendLog(`no.${i} nonce:${nonce} calc...`)
 			}
+			nonce++
+		}
 	}
-	console.log(`finish`)
-	console.timeEnd("ticker")
 	console.log(hashList)
+}
+
+/**
+ *
+ * @param {string} text log text
+ */
+function appendLog(text) {
+	/**
+	 * @type {HTMLTextAreaElement}
+	 */
+	//@ts-ignore
+	const element = document.getElementById("mining")
+	const time = new Date()
+
+	element.value += `${time.getHours().toString().padStart(2, "0")}:${time.getMinutes().toString().padStart(2, "0")}:${time.getSeconds().toString().padStart(2, "0")}.${time.getMilliseconds().toString().padStart(3, "0")}: ${text}\n`
+	element.scrollTo({ top: element.scrollHeight, behavior: "smooth" })
 }
